13af264d9dc014d218eba31d617ef7d790e92ffe
Andrew <tritiumgamingstudios@gmail.com>
2023-05-14 18:05:18 -0400

    Added API test

---
+++ src/ApiTest.tsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import {Fragment, useEffect, useState} from "react";
+
+function ApiTest() {
+
+    const [testResponse, setTestResponse] = useState<string>("-");
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/pilot")
+            .then(r=>{
+                console.log(r.data.answer)
+                setTestResponse(r.data.answer)
+            })
+    }, [])
+
+    return (
+        <Fragment>
+            <div>
+                <label style={{color:"cornflowerblue"}}>Local API response test:</label>
+                <div style={{color:"orangered", display:"flex"}}></div>
+                    <div style={{margin:'auto'}}>
+                        <table style={{width:'100%'}}>
+                            <tr><td><label>Correct response:</label></td><td>piloting</td></tr>
+                            <tr><td><label>Actual response:</label></td><td>{testResponse}</td></tr>
+                        </table>
+                    </div>
+                </div>
+            <label style={{color:"orangered"}}>Note: Flask must be running locally.</label>
+        </Fragment>
+    );
+}
+
+export default ApiTest;
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ApiTest from "./ApiTest";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,17 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Colorium</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ApiTest/>
     </>
   )
 }
