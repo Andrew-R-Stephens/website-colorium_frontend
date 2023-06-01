d224b4a0945cf146f4e4293821a9ec45324a5521
Andrew <tritiumgamingstudios@gmail.com>
2023-06-01 18:16:18 -0400

    + Refined visual behavior of infinite scrollbox + Expanded size of Hero banner + Refined HeroTitle animation

--- src/MinimizedPalette/MinimizedPaletteGallery.tsx
+++ src/MinimizedPalette/MinimizedPaletteGallery.tsx
@@ -86,6 +86,7 @@ function MinimizedPaletteGallery() {
                     </div>
                 </div>
             </div>
+            <div className={'minimized-palette-gallery-overlay'}/>
         </Fragment>
     )
 
