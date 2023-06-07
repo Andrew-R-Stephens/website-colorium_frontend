d5589f250b6ca22e55525e6d53ec2a5cccffd5b8
Steven Salmon <ssalmon7745@outlook.com>
2023-06-06 23:43:31 -0400

    Restructured landingpage files and renamed components

---
+++ src/LandingNav/LandingNav.tsx
@@ -0,0 +1,16 @@
+import './LandingNav.sass'
+function LandingNav(){
+    return(
+            <div className='nav-wrapper'>
+                <ul>
+                    <li className='nav-link'>Link 1</li>
+                    <li className='nav-link'>Link 2</li>
+                    <li className='nav-link'>Link 3</li>
+                    <li className='nav-link'>Link 4</li>
+                </ul>
+            </div >
+
+    )
+}
+
+export default LandingNav
